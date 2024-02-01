2432338708121abfaa0c9bd5d47dcbc031ada263
rebeccawaweru <wawerur95@gmail.com>
2024-02-01 14:48:44 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Change comes from within """
+
+
+def makeChange(coins, total):
+    """ Fewest number of coins needed for given amount """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
